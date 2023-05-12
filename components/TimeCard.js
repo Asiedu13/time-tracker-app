df5e140896706ae0f8e6aaea8773e9442b0becdf
prince <princekofasiedu@gmail.com>
2023-05-12 23:07:53 +0000

    Initial commit

---
+++ components/TimeCard.js
@@ -0,0 +1,21 @@
+export default function TimeCard() {
+
+    return (
+        <section className=''>
+            {/* Background image */}
+            <div></div>
+            {/* Time content */}
+            <section>
+                {/* Category and options */}
+                <div>
+
+                </div>
+                {/* Time - now and last week */}
+                <div>
+
+                </div>
+
+            </section>
+        </section>
+    )
+}
