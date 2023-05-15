d7873f86a80a1201cd971089c84f54cd74c3e59f
prince <princekofasiedu@gmail.com>
2023-05-15 16:35:02 +0000

    Started dynamic data inclusion

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
+import {useState, useEffect} from 'react'
 import ProfileCard from '../components/ProfileCard'
 import TimeCard from '../components/TimeCard'
 import { Rubik } from 'next/font/google'
+import jsonData from '../data.json' assert {type: 'json'}
 
-// const inter = Rubik()
+console.log(jsonData)
 const rubik = Rubik( {
   subsets: ['latin']
 })
 export default function Home() {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(()=> {
+    setUserData(jsonData)
+  }, [jsonData])
+
   return (
-    <main className={` w-[100vw] h-[100vh]  overflow-hidden flex justify-center items-center`}>
+    <main className={` w-[100vw] h-[100vh] flex justify-center items-center`}>
        <div className={rubik.className}>
 
         {/* Profile */}
@@ -16,9 +24,12 @@ export default function Home() {
           <ProfileCard />
         </section>
         {/* Personal time track */}
-        <section className='w-[fit-content] h-[50vh] flex flex-col justify-center items-center'>
-          <TimeCard />
-          <TimeCard />
+        <section className='w-[fit-content] h-[50vh] flex flex-col justify-center items-center relative overflow-x-auto'>
+          <div>
+
+          {userData.map(dat => <TimeCard timestamp={dat} key={Math.random()} />
+          )}
+          </div>
         </section>
        </div>
     </main>
